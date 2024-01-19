9c461060eb335c9fcda250237b9bcaae4eea8aaa
abeermosaad <abeermosaad69@gmail.com>
2024-01-19 16:56:53 +0200

    [Add] Tasks 3 & 4

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Comment: Create a table with glam rock bands
+SELECT band_name,
+  IF(split IS NOT NULL, split - formed, 2020 - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC, band_name DESC;
