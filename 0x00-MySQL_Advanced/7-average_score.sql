d8a4c3ecd93a5c7f10589cbb5af6125c5b01f9e1
abeermosaad <abeermosaad69@gmail.com>
2024-01-19 20:07:12 +0200

    [Edit] Tasks 7 & 8

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,13 @@
 -- creates a stored procedure AddBonus that adds a new correction for a student.
 
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN userid INT)
 BEGIN
 
-    SET @avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+    SET @avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = userid);
 
     UPDATE users SET average_score = @avg_score
-    WHERE id = user_id;
+    WHERE id = userid;
 END;
 //
 DELIMITER ;
